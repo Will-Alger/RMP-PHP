be6eacf0bcae235b27a103a69d2b369639cd9417
WilliamFancyS0n <WilliamAlger222@gmail.com>
2023-10-15 02:20:13 -0400

    added users

--- data/users.csv.php
+++ data/users.csv.php
@@ -1,2 +1,4 @@
 <?php die()
 test@test.com;$2y$10$Aqt1WBjU1orIviNIwNlDJe.PZJFfT27TyyaivV5882glGyJyZaWX6
+test45@test.com;$2y$10$vhF1C654vR76IhcnI680VOKEsUS.91VD2JZa2EUZeGLbuKTrHjyHW
+test3@test.com;$2y$10$bxN/CF3/dE6s28yauJ9W3.hk6ngvZij8kj.K0kADWgFs0bWeKXXaa
